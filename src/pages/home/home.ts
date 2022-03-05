cc452c10c28cdd762e755b7e21f538b2070a17a1
Alex <alex.rodrigues23997@gmail.com>
2022-03-05 15:59:17 -0300

    Tirando menu de slide da HomePage e ajustes visuais

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { MenuController, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,12 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
   login(){
     this.navCtrl.setRoot("EncomendasPage");
+  }
+
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
   }
 
 }
